ffdee75fb3dd1d6654d9a4824c34d58c7c58f7dc
Aditya Parab <2025.aditya.parab@ves.ac.in>
2026-02-20 23:05:27 +0530

    Delete students directory

--- students/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .models import Student
-
-class StudentTest(TestCase):
-
-    def test_student_creation(self):
-        student = Student.objects.create(name="Aditya", roll_number=101)
-        self.assertEqual(student.name, "Aditya")
